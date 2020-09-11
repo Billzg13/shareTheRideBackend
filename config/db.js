cb583efa5196ebaf78284ba11e7fd8f078314bab
Billzg13 <Billzg1234@gmail.com>
2020-09-11 16:19:42 +0300

    change port

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 module.exports = new Sequelize('sharetheride', 'root', '1q2w3e4r5t6y7u8i9o0p', {
   host: '18.195.215.56',
   dialect: 'mysql',
-  port: 3306,
+  port: 3310,
   define: {
     // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
     // This was true by default, but now is false by default
